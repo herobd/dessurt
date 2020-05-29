5e734083058d5df6a3c8121419c40b9310716f11
Brian Davis <herobd@gmail.com>
2020-05-29 11:09:30 -0600

    Finished coding hand-crafted features. Ready for testing

--- model/hand_code_emb.py
+++ model/hand_code_emb.py
@@ -4,9 +4,17 @@ import torch.nn as nn
 import numpy as np
 import re
 
-def read_list(filepath):
-    with open(filepath) as f:
-        lines = f.readlines()
+###
+#These features were crafted for the FUNSD (tobacco) dataset5
+###
+
+def read_list(filepaths):
+    if type(filepaths) is str:
+        filepaths=[filepaths]
+    lines=[]
+    for filepath is filepaths:
+        with open(filepath) as f:
+            lines += f.readlines()
     return set([l.lower().strip() for l in lines])
 
 class HandCodeEmb(nn.Module):
@@ -14,6 +22,48 @@ class HandCodeEmb(nn.Module):
         super(HandCodeEmb, self).__init__()
 
         self.name_list = read_list('data/text_lists/names_only_f300.txt')
+        self.place_list = read_list(['data/text_lists/states_and_cities.txt','data/text_lists/countries.txt'])
+        self.color_list = read_list('data/text_lists/colors.txt')
+        self.business_list = read_list('data/text_lists/businesss_terms.txt')
+        self.advertising_list = read_list('data/text_lists/advertisings_terms.txt')
+
+        self.feature_checks = [
+            hasNumeral,
+            hasMoney,
+            hasDayNumber,
+            hasYearNumber,
+            hasMonth,
+            hasDate,
+            hasDateNoYear,
+            hasTime,
+            hasWrittenNumber,
+            hasName,
+            hasPlace,
+            hasBusiness,
+            hasAd,
+            hasColor,
+            countCaps,
+            countNumerals,
+            countName,
+            isX,
+            noLetters,
+            questionName,
+            questionDate,
+            questionPlace,
+            questionNumber,
+            questionMoney,
+            questionTime
+            ]
+
+        self.num_feats = len(self.feature_checks)
+
+        self.emb_layers = nn.Sequential(
+                nn.Linear(self.num_feats,out_size),
+                nn.LeakyReLU(0.2,True),
+                nn.Linear(out_size,out_size),
+                nn.ReLU(True)
+                )
+
 
     
     def forward(self,transcriptions):
@@ -47,6 +97,8 @@ class HandCodeEmb(nn.Module):
         return 1 if re.search(r'(^|[^\w])1?\d[\/-][0123]?\d[\/-]([12]\d)?\d\d([^\w]|$)',s) is not None else 0
     def hasDateNoYear(self,s):
         return 1 if re.search(r'(^|[^\w])[01]?\d[\/-][0123]?\d([^\w]|$)',s) is not None else 0
+    def hasTime(self,s):
+        return 1 if re.search(r'(^|[^\w])[012]?\d\s?:\s?[0-5]?\d([^\w]|a|p|$)',s) is not None else 0
 
     def hasWrittenNumber(self,s):
         return 1 if re.search(r'(^|[^\w])(one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|\w+teen|twenty-?\w*|thirty-?\w*|fourty-?\w*|fifty-?\w*|sixty-?\w*|seventy-?\w*|eighty-?\w*|ninety-?\w*|hundred|thousand|million)([^\w]|$)',s,flags=re.IGNORECASE)) is not None else 0
@@ -78,12 +130,20 @@ class HandCodeEmb(nn.Module):
         return 1 if re.search(r'[a-zA-Z]',s) is None else 0
 
     def questionName(self,s):
-        return 1 if re.search(r'(^|[^\w])(requested by|requestor|editor|reporter|received by|leader|supervisor|manager|initialed by|submitted by|signed|recipient|prepared by|approved by|authors?|names?|signatures?|clients?|present|written by|investigators?)([^\w]|$)',s,flags=re.IGNORECASE)) is not None else 0
+        return 1 if re.search(r'(^|[^\w])(requested by|requestors?|editors?|reporters?|reported by|received by|leader|supervisors?|managers?|initialed by|submitted by|signed|recipients?|prepared by|approved by|authors?|names?|signatures?|clients?|present|written by|investigators?)([^\w]|$)',s,flags=re.IGNORECASE) is not None or re.match(r'(to|from):?',s,flags=re.IGNORECASE) is not None else 0
     def questionDate(self,s):
-        return 1 if re.search(r'(^|[^\w])(date|until|deadline)([^\w]|$)',s,flags=re.IGNORECASE)) is not None else 0
+        return 1 if re.search(r'(^|[^\w])(date|until|deadline|year|day|month)([^\w]|$)',s,flags=re.IGNORECASE) is not None else 0
     def questionPlace(self,s):
-        return 1 if re.search(r'(^|[^\w])(place|country|address)([^\w]|$)',s,flags=re.IGNORECASE)) is not None else 0
+        return 1 if re.search(r'(^|[^\w])(city|place|country|address|location|markets?|zones?)([^\w]|$)',s,flags=re.IGNORECASE) is not None else 0
     def questionNumber(self,s):
-        return 1 if re.search(r'score|number|length|weight|rate|width|density|#|quantity|no\.',s,flags=re.IGNORECASE)) is not None else 0
+        return 1 if re.search(r'score|number|length|weight|rate|width|density|volume|circumference|#|quantity|no\.|count|code|phone',s,flags=re.IGNORECASE) is not None else 0
     def questionMoney(self,s):
-        return 1 if re.search(r'(^|[^\w])(costs?|paid|budgets?|balance|\$)([^\w]|$)',s,flags=re.IGNORECASE)) is not None else 0
+        return 1 if re.search(r'(^|[^\w])(costs?|paid|budgets?|balances?|\$|values?|prices?)([^\w]|$)',s,flags=re.IGNORECASE) is not None else 0
+    def questionTime(self,s):
+        return 1 if re.search(r'(^|[^\w])(time|hours?)([^\w]|$)',s,flags=re.IGNORECASE) is not None else 0
+
+
+
+
+
+
