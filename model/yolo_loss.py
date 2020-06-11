e7c18b810e29165bbe1165143add083ab3781456
Brian Davis <herobd@gmail.com>
2020-06-11 17:22:26 -0600

    more work on new loss

--- model/yolo_loss.py
+++ model/yolo_loss.py
@@ -65,89 +65,84 @@ def build_oversegmented_targets_no_anchors_multiscale(
                         draw[:,i*VIZ_SIZE,:]=60
                     for j in range(0,nH):
                         draw[j*VIZ_SIZE,:,:]=60
+                    draw[:,:,0]=90
                     draws.append(draw)
 
                 draw_colors = [(255,0,0),(100,255,0),(255,100,0),(0,255,0),(200,200,0),(255,200,0),(200,255,0)]
 
 
             #candidate_pos=[]
-            #We need to decide which scale this is at TODO
-
+            #We need to decide which scale this is at.
+            # I think the best is to simply choose the tile height that is closes to the (rotated) bb height
+            closest_diff = 9999999999
             for level in range(len(nHs)):
 
-                gx = target[b, t, 0] / scale[level][0]
-                gy = target[b, t, 1] / scale[level][1]
-                gw = target[b, t, 4] / scale[level][0]
+                gr = target[b, t, 2]
                 gh = target[b, t, 3] / scale[level][1]
 
-                gx1 = gx-gw
-                gx2 = gx+gw
-                gy1 = gy-gh
-                gy2 = gy+gh
-            
-                if gw==0 or gh==0:
-                    continue
-                # Get grid box indices
-                gi = max(min(int(gx),conf_mask.size(3)-1),0)
-                gj = max(min(int(gy),conf_mask.size(2)-1),0)
-                gi1 = max(min(int(gx1),conf_mask.size(3)-1),0)
-                gj1 = max(min(int(gy1),conf_mask.size(2)-1),0)
-                gi2 = max(min(int(gx2),conf_mask.size(3)-1),0)
-                gj2 = max(min(int(gy2),conf_mask.size(2)-1),0)
-                #We truncate with int() instead of rounding since each tile i is actually centered at i+0.5
+                rh = gh #math.cos(gr)*gh this isn't quite what I wanted...
+                diff = abs(rh-1)
+                if diff<closest_diff:
+                    closest_diff = diff
+                    closest = level
+            level = closest
 
-                #We don't want to include a tile if the real box doesn't extend past it's centerpoint, these shouldn't predict (arguably unless the real box covers the whole tile we don't want to predict)
-                if gx1>gi1+0.5:
-                    gi1+=1
-                if gx2<gi2+0.5:
-                    gi2-=1
+            nH = nHs[level]
+            nW = nWs[level]
 
-                #We need to handle the end points of the line differently (they probably need smaller anchor rectangles)
-                if gi1>gi2: #uh oh, we have a really small box between two tiles
-                    gi1=gi2 = gi
+            gx = target[b, t, 0] / scale[level][0]
+            gy = target[b, t, 1] / scale[level][1]
+            gr = target[b, t, 2]
+            gw = target[b, t, 4] / scale[level][0]
+            gh = target[b, t, 3] / scale[level][1]
 
-                if gi1==gi2:
-                    over_seg_gws = [gw]
-                else:
-                    
-                    #Get best matching anchor
-                    #Build oversegmented gt sizes for each i/tile: (gh,min(self.maxWidth,this_x-gx1,gx2-this_x))
-                    over_seg_gws = [min(max_width,this_i+0.5-gx1,gx2-(this_i+0.5)) for this_i in range(gi1,gi2+1)]
-                best_ns,anch_ious = multi_get_closest_anchor_iou(anchors,gh,over_seg_gws)
+            g_lx = target[b, t, 5] / scale[level][0]
+            g_ly = target[b, t, 6] / scale[level][1]
+            g_rx = target[b, t, 7] / scale[level][0]
+            g_ry = target[b, t, 8] / scale[level][1]
+            g_tx = target[b, t, 9] / scale[level][0]
+            g_ty = target[b, t, 10] / scale[level][1]
+            g_bx = target[b, t, 11] / scale[level][0]
+            g_by = target[b, t, 12] / scale[level][1]
 
-            #best_n, anch_ious = get_closest_anchor_iou(anchors,gh,min(gw,self.maxWidth))
-            # Where the overlap is larger than threshold set mask to zero (ignore)
-            #conf_mask[b, anch_ious > ignore_thres, gj, gi] = 0
-            #  ignore_range, all, set to 1 later
-            anch_x,ignore_anch = torch.where(anch_ious > ignore_thresh)
-            #conf_mask[([b]*anch_x.size(0),ignore_anch,list(range(gj1:gj2+1)),anch_x)]=0
-            for j in range(gj1,gj2+1):
-                conf_mask[b,:,j,:][ignore_anch,anch_x]=0
-                if VISUAL_DEBUG:
-                    for iii in range( anch_x.size(0) ):
-                        draw[j*VIZ_SIZE+1:(j+1)*VIZ_SIZE,anch_x[iii]*VIZ_SIZE+ignore_anch[iii]*anchor_draw_size:anch_x[iii]*VIZ_SIZE+(ignore_anch[iii]+1)*anchor_draw_size,1]=0
-            #conf_mask[b, anch_ious > ignore_thres, gj1:gj2+1,gi1:gi2+1] = 0
-            # Get ground truth box
-            gt_box = torch.FloatTensor(np.array([gx, gy, gw, gh])).unsqueeze(0)
-            gt_area = gw+gh
+            gt_area = gw*gh
+
+            #gx1 = gx-gw
+            #gx2 = gx+gw
+            #gy1 = gy-gh
+            #gy2 = gy+gh
+        
+            if gw==0 or gh==0:
+                continue
+            #What tiles are relevant for predicting this? Just the center line? Do any tiles get an ignore (unmask)?
+            hit_tile_ys, hit_tile_xs = skimage.draw.line(int(g_ly),int(g_lx),int(g_ry),int(g_rx))
+            ignore_tile_ys, ignore_tile_xs, weight_tile_ys, weight_tile_ys = skimage.draw.line(int(g_ly),int(g_lx),int(g_ry),int(g_rx)) #This is nice, except I'm forced to pass integers in, perhaps this could be used for ignoring?
+
+            #all_tile_ys, all_tile_xs = skimage.draw.polygon(r,c,(nH,nW))
+
+            #TODO fix end points. Probably shouldn't include tile if we're barely on it.
+
+            conf_mask[b,ignore_tile_ys,ignore_tile_xs]=0
+            if VISUAL_DEBUG:
+                for ty,tx in zip(ignore_tile_ys,ignore_tile_xs):
+                    draws[level][ty*VIZ_SIZE+1:(ty+1)*VIZ_SIZE,tx*VIZ_SIZE+1:(tx+1)*VIZ_SIZE,0]=0
             # Get the best prediction
             #pred_box = pred_boxes[b, best_n, gj, gi].unsqueeze(0)
             # Masks
 
-            mask[b,:,gj,:][(best_ns,list(range(gi1,gi2+1)))] = 1
-            conf_mask[b,:,gj,:][(best_ns,list(range(gi1,gi2+1)))] = 1
+            mask[b,hit_tile_ys,hit_tile_xs]=1
+            conf_mask[b,hit_tile_ys,hit_tile_xs]=1
             if VISUAL_DEBUG:
-                for anchor_n, i in zip(best_ns,list(range(gi1,gi2+1))):
-                    draw[gj*VIZ_SIZE+1:(gj+1)*VIZ_SIZE,i*VIZ_SIZE+anchor_n*anchor_draw_size:i*VIZ_SIZE+(anchor_n+1)*anchor_draw_size,0]=110
-                    draw[gj*VIZ_SIZE+1:(gj+1)*VIZ_SIZE,i*VIZ_SIZE+anchor_n*anchor_draw_size:i*VIZ_SIZE+(anchor_n+1)*anchor_draw_size,1]=110
+                for ty,tx in zip(hit_tile_ys,hit_tile_xs):
+                    draws[level][ty*VIZ_SIZE+1:(ty+1)*VIZ_SIZE,tx*VIZ_SIZE+1:(tx+1)*VIZ_SIZE,0:2]=110
             #mask[b, best_n, gj, gi1:gi2+1] = 1
             #conf_mask[b, best_n, gj, gi1:gi2+1] = 1 #we ned to set this to 1 as we ignored it earylier
             # Coordinates
             #DO we first want to compute position and than the scaling based on that, or vice-versa? Always  trying to predict at the edge of a tile might lead to weird effects... What about random for each instance? Or halfway for each?
             #-> I think my final verdict will be to move and strech such that the side you're not matching stays in the same place. This should always maintain a constant distance form the center of the predicting tile to the max length for open/continuing sides
             #For X, the anchor was selected assuming a position centered on the tile. However, this won't work for some (end tiles). We can simply compute the best position given the selected anchors
-            anchor_width = anchors[:,0]
             for index in range(len(best_ns)):
+            for ty,tx in zip(hit_tile_ys,hit_tile_xs):
                 i = index+gi1
                 best_n = best_ns[index]
                 diff1 = gx1-(i+0.5-anchor_width[best_n])
