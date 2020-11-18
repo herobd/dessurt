cb645ec59e1fab8bb26e0e0b614e5761a70804c3
Brian Davis <herobd@gmail.com>
2020-11-18 10:37:12 -0700

    readd assert. remove hack fix

--- model/oversegment_loss.py
+++ model/oversegment_loss.py
@@ -1127,17 +1127,17 @@ def build_oversegmented_targets_multiscale(
                             L = max(min(L,MAX_W_PRED-0.01),0.01-MAX_W_PRED)
                             R=ri_x-tile_x 
                             R = max(min(R,MAX_W_PRED-0.01),0.01-MAX_W_PRED)
-                            if gw<0.1 and R<L:
-                                #this is a hack
-                                tmp=R
-                                R=L
-                                L=tmp
+                            #if gw<0.1 and R<L:
+                            #    #this is a hack
+                            #    tmp=R
+                            #    R=L
+                            #    L=tmp
                             targ_L[b, assigned, cell_y, cell_x] = inv_tanh(L/MAX_W_PRED)
                             targ_R[b, assigned, cell_y, cell_x] = inv_tanh(R/MAX_W_PRED)
 
                             if not (R>=L and T<=B):
                                 print('WARNING/ overseg loss L:{}, R:{}, T:{}, B:{}'.format(L,R,T,B))
-                            #assert(R>=L and T<=B)
+                            assert(R>=L and T<=B)
                         else:
                             #T=ti_y-tile_y #negative if above tile center (just add predcition to center)
                             ##T = max(min(T,MAX_H_PRED-0.01),0.01-MAX_H_PRED)
@@ -1168,16 +1168,16 @@ def build_oversegmented_targets_multiscale(
                             R=bi_y-tile_y 
                             R = max(min(R,MAX_H_PRED-0.01),0.01-MAX_H_PRED)#this clips the max heigh it can predict
                             #assert(abs(R)<MAX_H_PRED)
-                            if gw<0.1 and R<L:
-                                #this is a hack
-                                tmp=R
-                                R=L
-                                L=tmp
+                            #if gw<0.1 and R<L:
+                            #    #this is a hack
+                            #    tmp=R
+                            #    R=L
+                            #    L=tmp
                             targ_L[b, assigned, cell_y, cell_x] = inv_tanh(L/MAX_H_PRED)
                             targ_R[b, assigned, cell_y, cell_x] = inv_tanh(R/MAX_H_PRED)
                             if not (R>=L and T<=B):
                                 print('WARNING/ overseg loss L:{}, R:{}, T:{}, B:{}'.format(L,R,T,B))
-                            #assert(R>=L and T<=B)
+                            assert(R>=L and T<=B)
                         #t#times_assign.append(timeit.default_timer()-tic2)
 
 
