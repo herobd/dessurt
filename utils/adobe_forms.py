d3c7612d8591e277e1640ed0e1608d206a266027
Brian Davis <herobd@gmail.com>
2021-01-07 17:45:01 -0700

    work on Adobe dataset

--- utils/adobe_forms.py
+++ utils/adobe_forms.py
@@ -135,80 +135,147 @@ def parseGT(directory,name):
     for bb_i in range(len(all_bbs)):
         if bb_i not in belongs_to:
             printRec(bb_i,0)
-    ##I think these are the only things needing grouped
-    #A widget in a text block is fill-in-the-blank!!!!
-    #groups=defaultdict(list)
-    #if 'TextBlock' in data:
-    #    for i,bb in enumerate(data['TextBlock']):
-    #        bb_i = all_bbs.index(('TextBlock',i))
-    #        for other_bb_i in owns[bb_i]:
-    #            ssert(all_bbs[other_bb_i][0]=='TextRun') #could be widget
-    #            groups[bb_i].append(other_bb_i)
+    #I think these are the only things needing grouped
+    A widget in a text block is fill-in-the-blank!!!!
+    groups=defaultdict(list)
+    if 'TextBlock' in data:
+        for i,bb in enumerate(data['TextBlock']):
+            bb_i = all_bbs.index(('TextBlock',i))
+            for other_bb_i in owns[bb_i]:
+                ssert(all_bbs[other_bb_i][0]=='TextRun') #could be widget
+                groups[bb_i].append(other_bb_i)
 
-    ##linking
-    #if 'ChoiceGroup' in data:
-    #    for i,bb in enumerate(data['ChoiceGroup']):
-    #        bb_i = all_bbs.index(('ChoiceGroup',i))
-    #        title_i = None
-    #        link_to_title=[]
-    #        for other_bb_i in owns[bb_i]:
-    #            if 'Title' in all_bbs[other_bb_i][0]:
-    #                assert title_i is None
-    #                title_i = other_bb_i
-    #            elif len(owns[other_bb_i])>0:
-    #                link_to_title.append(other_bb_i)
-    #            else:
-    #                print('unknown in choice group {} {}'.format(other_bb_i,all_bbs[other_bb_i]))
-    #                import pdb;pdb.set_trace() #not sure what this is
+    #linking
 
-    #        for other_bb_i in link_to_title:
-    #    
+    def linkField(bb_i):
+        assert len(owns[bb_i])==2
+        text_i=None
+        widget_i=None
+        for o_bb_i in owns[bb_i]:
+            if all_bbs[o_bb_i][0]=='TextBlock':
+                text_i = o_bb_i
+            elif all_bbs[o_bb_i][0]=='Widget':
+                widget_i = o_bb_i
 
-    #    
-    #for json_type in ["Widget","TextRun"]:
-    #    if json_type in data:
-    #        print(json_type)
-    #        for i,bb in enumerate(data[json_type]):
-    #            bb_i = all_bbs.index((json_type,i))
-    #            x=round(bb['x'])
-    #            y=round(bb['y'])
-    #            w=round(bb['w'])
-    #            h=round(bb['h'])
-    #            t=bb['jsonClass']
+        assert(len(owns[widget_i])==0)
+        text_top, text_bot = linkTextBlock(text_i,pairs)
+        #how to know which is first? A widget could be on any side. What if it's below to the left?
+        #were going to pair based on order. left most (widget or text) pairs up
+        if 
 
-    #            x2=x+w
-    #            y2=y+h
+    def linkChoiceField(bb_i,pairs):
+        assert len(owns[bb_i])==1 and all_bbs[owns[bb_i][0]][0]=='Field'
+        return linkField(bb_i,pairs)
+    def linkChoiceGroup(bb_i,pairs):
+        title_i = None
+        to_link_pre=[]
+        for other_bb_i in owns[bb_i]:
+            if 'ChoiceGroupTitle' == all_bbs[other_bb_i][0]:
+                assert title_i is None
+                title_i = other_bb_i
+            elif len(owns[other_bb_i])>0:
+                to_link_pre.append(other_bb_i)
+            else:
+                print('unknown in choice group {} {}'.format(other_bb_i,all_bbs[other_bb_i]))
+                import pdb;pdb.set_trace() #not sure what this is
 
-    #            #linking and grouping
+        to_link_to_ChoiceField=[]
+        to_link_post=[]
+        for other_bb_i in to_link_pre:
+            if 'ChoiceField' == all_bbs[other_bb_i][0]:
+                to_link_bb_i = linkChoiceField(other_bb_i,pairs)
+                to_link_post.append(to_link_bb_i)
+            elif 'ChoiceGroup' == all_bbs[other_bb_i][0]:
+                to_link_bb_is = linkChoiceGroup(other_bb_i,pairs)
+                #but these probably need linked to a ChoiceField...
+                #closest to top-and-left?
+                to_link_to_ChoiceField.append(to_link_bb_is)
+            else:
+                print('Error, {} in a ChoiceGroup'.format( all_bbs[other_bb_i][0]))
+                assert False
 
-    #            #bounding box
-    #            lXL=x
-    #            rXL=x2
-    #            tYL=y
-    #            bYL=y2
-    #            s=scale
-    #            bb = np.empty(8+8+numClasses, dtype=np.float32)
-    #            bb[0]=lXL*s
-    #            bb[1]=tYL*s
-    #            bb[2]=rXL*s
-    #            bb[3]=tYL*s
-    #            bb[4]=rXL*s
-    #            bb[5]=bYL*s
-    #            bb[6]=lXL*s
-    #            bb[7]=bYL*s
-    #            #we add these for conveince to crop BBs within window
-    #            bb[8]=s*lXL
-    #            bb[9]=s*(tYL+bYL)/2.0
-    #            bb[10]=s*rXL
-    #            bb[11]=s*(tYL+bYL)/2.0
-    #            bb[12]=s*(lXL+rXL)/2.0
-    #            bb[13]=s*tYL
-    #            bb[14]=s*(rXL+lXL)/2.0
-    #            bb[15]=s*bYL
+        for bb_is in to_link_to_ChoiceField:
+            for bb_i in bb_is:
+                xs.append(x1s[bb_i])
+                ys.append(y1s[bb_i])
+            xs=np.array(xs)
+            ys=np.array(ys)
+            xg = xs.min()
+            yg = yx.min()
 
-    #            bb[16:]=0
-    #            bb[classMap[label]]=1
-    #            bbs.append(bb)
+            min_dist=9999999
+            best_bb_i=None
+            for bb_i in to_link_post:
+                x = x1s[bb_i]
+                y = y1s[bb_i]
+                if y<=yg or x<=xg:
+                    dist = abs(y-yg)+abs(x-xg)
+                    if dist<min_dist:
+                        min_dist=dist
+                        best_bb_i = bb_i
+
+            for bb_i in bb_is:
+                pairs.append((min(bb_i,best_bb_i),max(bb_i,best_bb_i)))
+
+        if title_i is not None:
+            for to_link_bb_i in to_link_post:
+                pairs.add((min(title_i,to_link_bb_i),max(title_i,to_link_bb_i)))
+            return [title_i]
+        else:
+            return to_link_post
+
+    if 'ChoiceGroup' in data:
+        for i,bb in enumerate(data['ChoiceGroup']):
+            bb_i = all_bbs.index(('ChoiceGroup',i))
+            linkChoiceGroup(bb_i,pairs)
+
+        
+
+        
+    for json_type in ["Widget","TextRun"]:
+        if json_type in data:
+            print(json_type)
+            for i,bb in enumerate(data[json_type]):
+                bb_i = all_bbs.index((json_type,i))
+                x=round(bb['x'])
+                y=round(bb['y'])
+                w=round(bb['w'])
+                h=round(bb['h'])
+                t=bb['jsonClass']
+
+                x2=x+w
+                y2=y+h
+
+                #linking and grouping
+
+                #bounding box
+                lXL=x
+                rXL=x2
+                tYL=y
+                bYL=y2
+                s=scale
+                bb = np.empty(8+8+numClasses, dtype=np.float32)
+                bb[0]=lXL*s
+                bb[1]=tYL*s
+                bb[2]=rXL*s
+                bb[3]=tYL*s
+                bb[4]=rXL*s
+                bb[5]=bYL*s
+                bb[6]=lXL*s
+                bb[7]=bYL*s
+                #we add these for conveince to crop BBs within window
+                bb[8]=s*lXL
+                bb[9]=s*(tYL+bYL)/2.0
+                bb[10]=s*rXL
+                bb[11]=s*(tYL+bYL)/2.0
+                bb[12]=s*(lXL+rXL)/2.0
+                bb[13]=s*tYL
+                bb[14]=s*(rXL+lXL)/2.0
+                bb[15]=s*bYL
+
+                bb[16:]=0
+                bb[classMap[label]]=1
+                bbs.append(bb)
 
     print(list(data.keys()))
 
