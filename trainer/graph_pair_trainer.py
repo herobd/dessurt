0bfa765e5d463730293b0698d77f6474d455d1df
Brian Davis <herobd@gmail.com>
2021-02-13 23:51:36 -0700

    ?

--- trainer/graph_pair_trainer.py
+++ trainer/graph_pair_trainer.py
@@ -1966,11 +1966,12 @@ class GraphPairTrainer(BaseTrainer):
                     candidate_lists[gtG1].append((-1,False))
                 elif (class1=='header' and class0=='question') or (class1=='question' and class0=='answer'):
                     candidate_lists[gtG0].append((-1,False))
-                elif class0=='question'  and class1=='question':
-                    #IDK
-                    candidate_lists[gtG0].append((-1,False))
+                #elif class0=='question'  and class1=='question':
+                #    #IDK
+                #    candidate_lists[gtG0].append((-1,False))
                 else:
-                    assert False
+                    candidate_lists[gtG0].append((-1,False))
+                    #assert False
 
             sum_ap=0
             hit_at_1=0
