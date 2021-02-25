965625ea1b45f9fccb16bfd42c7751c1ac3c9d59
Brian Davis <herobd@gmail.com>
2021-02-25 10:37:52 -0700

    fix to draw

--- trainer/graph_pair_trainer.py
+++ trainer/graph_pair_trainer.py
@@ -1927,6 +1927,7 @@ class GraphPairTrainer(BaseTrainer):
                             image,
                             edgePredTypes,
                             missedRels,
+                            None,
                             targetBoxes,
                             self.classMap,
                             path,
