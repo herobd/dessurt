4f17f146ba27ed65ea8375f854a9a512146f9d5e
Brian Davis <herobd@gmail.com>
2021-04-16 13:26:09 -0600

    fixes to label_smoothing. Fixes to new test model

--- datasets/nobrain_graph_pair.py
+++ datasets/nobrain_graph_pair.py
@@ -1,6 +1,6 @@
 import torch.utils.data
 import numpy as np
-import json
+import json, re
 #from skimage import io
 #from skimage import draw
 #import skimage.transform as sktransform
@@ -34,7 +34,13 @@ class NobrainGraphPair(GraphPairDataset):
         self.images=[]
         self.images.append({'id':'0', 'imagePath':'../data/FUNSD/training_data/images/12825369.png', 'annotationPath':'../data/english_char_set.json', 'rescaled':1.0, 'imageName':'0'})
 
-
+        if 'textfile' in config:
+            with open(config['textfile']) as f:
+                text = f.read()
+            text=re.sub('\s+',' ',text)
+            self.words = text.strip().split(' ')
+        else:
+            self.words = None
 
 
 
@@ -43,104 +49,165 @@ class NobrainGraphPair(GraphPairDataset):
 
         bbs = np.empty((1,0, 8+8+numClasses), dtype=np.float32) #2x4 corners, 2x4 cross-points, n classes
 
+
         word_boxes=[]
         word_trans=[]
 
-        bb=[None]*16
-        lX=0
-        rX=10
-        tY=0
-        bY=10
-        bb[0]=lX*s
-        bb[1]=bY*s
-        bb[2]=lX*s
-        bb[3]=tY*s
-        bb[4]=rX*s
-        bb[5]=tY*s
-        bb[6]=rX*s
-        bb[7]=bY*s
-        bb[8]=s*(lX+rX)/2.0
-        bb[9]=s*bY
-        bb[10]=s*(lX+rX)/2.0
-        bb[11]=s*tY
-        bb[12]=s*lX
-        bb[13]=s*(tY+bY)/2.0
-        bb[14]=s*rX
-        bb[15]=s*(tY+bY)/2.0
-        word_boxes.append(bb)
-        word_trans.append('name:')
+        if self.words is None:
+            bb=[None]*16
+            lX=0
+            rX=10
+            tY=0
+            bY=10
+            bb[0]=lX*s
+            bb[1]=bY*s
+            bb[2]=lX*s
+            bb[3]=tY*s
+            bb[4]=rX*s
+            bb[5]=tY*s
+            bb[6]=rX*s
+            bb[7]=bY*s
+            bb[8]=s*(lX+rX)/2.0
+            bb[9]=s*bY
+            bb[10]=s*(lX+rX)/2.0
+            bb[11]=s*tY
+            bb[12]=s*lX
+            bb[13]=s*(tY+bY)/2.0
+            bb[14]=s*rX
+            bb[15]=s*(tY+bY)/2.0
+            word_boxes.append(bb)
+            word_trans.append('name:')
 
-        bb=[None]*16
-        lX=10
-        rX=20
-        tY=0
-        bY=10
-        bb[0]=lX*s
-        bb[1]=bY*s
-        bb[2]=lX*s
-        bb[3]=tY*s
-        bb[4]=rX*s
-        bb[5]=tY*s
-        bb[6]=rX*s
-        bb[7]=bY*s
-        bb[8]=s*(lX+rX)/2.0
-        bb[9]=s*bY
-        bb[10]=s*(lX+rX)/2.0
-        bb[11]=s*tY
-        bb[12]=s*lX
-        bb[13]=s*(tY+bY)/2.0
-        bb[14]=s*rX
-        bb[15]=s*(tY+bY)/2.0
-        word_boxes.append(bb)
-        word_trans.append('Skynet')
+            bb=[None]*16
+            lX=10
+            rX=20
+            tY=0
+            bY=10
+            bb[0]=lX*s
+            bb[1]=bY*s
+            bb[2]=lX*s
+            bb[3]=tY*s
+            bb[4]=rX*s
+            bb[5]=tY*s
+            bb[6]=rX*s
+            bb[7]=bY*s
+            bb[8]=s*(lX+rX)/2.0
+            bb[9]=s*bY
+            bb[10]=s*(lX+rX)/2.0
+            bb[11]=s*tY
+            bb[12]=s*lX
+            bb[13]=s*(tY+bY)/2.0
+            bb[14]=s*rX
+            bb[15]=s*(tY+bY)/2.0
+            word_boxes.append(bb)
+            word_trans.append('Skynet')
 
-        bb=[None]*16
-        lX=0
-        rX=10
-        tY=10
-        bY=20
-        bb[0]=lX*s
-        bb[1]=bY*s
-        bb[2]=lX*s
-        bb[3]=tY*s
-        bb[4]=rX*s
-        bb[5]=tY*s
-        bb[6]=rX*s
-        bb[7]=bY*s
-        bb[8]=s*(lX+rX)/2.0
-        bb[9]=s*bY
-        bb[10]=s*(lX+rX)/2.0
-        bb[11]=s*tY
-        bb[12]=s*lX
-        bb[13]=s*(tY+bY)/2.0
-        bb[14]=s*rX
-        bb[15]=s*(tY+bY)/2.0
-        word_boxes.append(bb)
-        word_trans.append('Month:')
+            bb=[None]*16
+            lX=0
+            rX=10
+            tY=10
+            bY=20
+            bb[0]=lX*s
+            bb[1]=bY*s
+            bb[2]=lX*s
+            bb[3]=tY*s
+            bb[4]=rX*s
+            bb[5]=tY*s
+            bb[6]=rX*s
+            bb[7]=bY*s
+            bb[8]=s*(lX+rX)/2.0
+            bb[9]=s*bY
+            bb[10]=s*(lX+rX)/2.0
+            bb[11]=s*tY
+            bb[12]=s*lX
+            bb[13]=s*(tY+bY)/2.0
+            bb[14]=s*rX
+            bb[15]=s*(tY+bY)/2.0
+            word_boxes.append(bb)
+            word_trans.append('Month:')
 
-        bb=[None]*16
-        lX=10
-        rX=20
-        tY=10
-        bY=20
-        bb[0]=lX*s
-        bb[1]=bY*s
-        bb[2]=lX*s
-        bb[3]=tY*s
-        bb[4]=rX*s
-        bb[5]=tY*s
-        bb[6]=rX*s
-        bb[7]=bY*s
-        bb[8]=s*(lX+rX)/2.0
-        bb[9]=s*bY
-        bb[10]=s*(lX+rX)/2.0
-        bb[11]=s*tY
-        bb[12]=s*lX
-        bb[13]=s*(tY+bY)/2.0
-        bb[14]=s*rX
-        bb[15]=s*(tY+bY)/2.0
-        word_boxes.append(bb)
-        word_trans.append('May')
+            bb=[None]*16
+            lX=10
+            rX=20
+            tY=10
+            bY=20
+            bb[0]=lX*s
+            bb[1]=bY*s
+            bb[2]=lX*s
+            bb[3]=tY*s
+            bb[4]=rX*s
+            bb[5]=tY*s
+            bb[6]=rX*s
+            bb[7]=bY*s
+            bb[8]=s*(lX+rX)/2.0
+            bb[9]=s*bY
+            bb[10]=s*(lX+rX)/2.0
+            bb[11]=s*tY
+            bb[12]=s*lX
+            bb[13]=s*(tY+bY)/2.0
+            bb[14]=s*rX
+            bb[15]=s*(tY+bY)/2.0
+            word_boxes.append(bb)
+            word_trans.append('May')
+        else:
+            self.qa=[]
+            cY=0
+            for i in range(self.questions):
+                words = random.sample(self.words,k=4)
+                q = words[0]+' '+words[1]
+                a = words[2]+' '+words[3]
+
+                bb=[None]*16
+                lX=0
+                rX=10
+                tY=cY
+                bY=cY+10
+                bb[0]=lX*s
+                bb[1]=bY*s
+                bb[2]=lX*s
+                bb[3]=tY*s
+                bb[4]=rX*s
+                bb[5]=tY*s
+                bb[6]=rX*s
+                bb[7]=bY*s
+                bb[8]=s*(lX+rX)/2.0
+                bb[9]=s*bY
+                bb[10]=s*(lX+rX)/2.0
+                bb[11]=s*tY
+                bb[12]=s*lX
+                bb[13]=s*(tY+bY)/2.0
+                bb[14]=s*rX
+                bb[15]=s*(tY+bY)/2.0
+                word_boxes.append(bb)
+                word_trans.append(q)
+
+                bb=[None]*16
+                lX=10
+                rX=20
+                tY=cY
+                bY=cY+10
+                bb[0]=lX*s
+                bb[1]=bY*s
+                bb[2]=lX*s
+                bb[3]=tY*s
+                bb[4]=rX*s
+                bb[5]=tY*s
+                bb[6]=rX*s
+                bb[7]=bY*s
+                bb[8]=s*(lX+rX)/2.0
+                bb[9]=s*bY
+                bb[10]=s*(lX+rX)/2.0
+                bb[11]=s*tY
+                bb[12]=s*lX
+                bb[13]=s*(tY+bY)/2.0
+                bb[14]=s*rX
+                bb[15]=s*(tY+bY)/2.0
+                word_boxes.append(bb)
+                word_trans.append(a)
+
+                self.qa.append((q,a,None))
+
+                cY+=11
 
         word_boxes = np.array(word_boxes)
         trans = []
@@ -164,6 +231,8 @@ class NobrainGraphPair(GraphPairDataset):
             return cto
 
     def makeQuestions(self,bbs,transcription,groups,groups_adj):
+        if self.words is not None:
+            return self.qa
         new_all_q_a=[]
         new_all_q_a.append(('name:','skynet',None))
         new_all_q_a.append(('month:','may',None))
