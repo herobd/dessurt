60480ac33461853e5881af3d94da603a59ca6cf2
Brian Davis <herobd@gmail.com>
2021-04-21 13:53:04 -0600

    changed dataset again

--- datasets/nobrain_graph_pair.py
+++ datasets/nobrain_graph_pair.py
@@ -39,7 +39,7 @@ class NobrainGraphPair(GraphPairDataset):
                 text = f.read()
         else:
             #text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque faucibus ligula accumsan dui hendrerit, sit amet egestas risus suscipit. Mauris vel euismod lacus. Quisque fermentum sed tortor eleifend congue. Donec at odio non diam rutrum posuere at bibendum erat. Sed id tempus ligula. Proin nec elit eget ligula dignissim varius a in libero. Integer scelerisque sem iaculis magna lobortis, non porta dui auctor. Integer efficitur quam vel ex fermentum sagittis. Donec quis pharetra mi. Vestibulum fringilla vitae tellus vel volutpat. Nam augue felis, lacinia quis mi a, semper dictum justo. Maecenas varius sollicitudin augue, nec pellentesque orci faucibus et. Praesent eget quam nibh. Fusce euismod neque sit amet mollis finibus. Cras posuere purus non diam cursus vestibulum. Cras ut posuere diam, et facilisis eros.'
-            text='this is some sample text and should be very easy to solve just do it already please I am rather frustrated with you music dog cat train girl boy food'
+            text='this is some sample text and should be very easy to solve just do it already please I am rather frustrated with you music dog cat train girl boy food pants shirt table chair bed go come take leave off on apart together close far big small round smooth flat touch press hot cold warm cool floor wall red blue green purple pink yellow orange hair long short spear sword grab pull push hard soft her his high low ask'
         text=re.sub('\s+',' ',text)
         self.words = text.strip().lower().split(' ')
         #else:
@@ -162,9 +162,11 @@ class NobrainGraphPair(GraphPairDataset):
             cY=0
             questions=[]
             skipped=[]
-            q_s = random.sample(self.words,k=self.questions)
-            a_s = random.sample(self.words,k=self.questions)
-            for q,a in zip(q_s,a_s):
+            q_s = random.sample(self.words,k=2*self.questions)
+            a_s = random.sample(self.words,k=2*self.questions)
+            for i in range(self.questions):
+                q=q_s[i]
+                a=a_s[i]
                 if random.random()<self.not_present_freq:
                     skipped.append(q)
                 else:
@@ -233,6 +235,63 @@ class NobrainGraphPair(GraphPairDataset):
                     self.qa.append((q,a,None))
 
                     cY+=11
+
+            #keep document size the same by adding redherring words
+            for i in range(len(skipped)):
+                q=q_s[-(i+1)]
+                a=a_s[-(i+1)]
+                bb=[None]*16
+                lX=0
+                rX=10
+                tY=cY
+                bY=cY+10
+                bb[0]=lX*s
+                bb[1]=bY*s
+                bb[2]=lX*s
+                bb[3]=tY*s
+                bb[4]=rX*s
+                bb[5]=tY*s
+                bb[6]=rX*s
+                bb[7]=bY*s
+                bb[8]=s*(lX+rX)/2.0
+                bb[9]=s*bY
+                bb[10]=s*(lX+rX)/2.0
+                bb[11]=s*tY
+                bb[12]=s*lX
+                bb[13]=s*(tY+bY)/2.0
+                bb[14]=s*rX
+                bb[15]=s*(tY+bY)/2.0
+                word_boxes.append(bb)
+                if self.only_present:
+                    word_trans.append(q)
+                else:
+                    word_trans.append('['+q+']')
+
+                    bb=[None]*16
+                    lX=10
+                    rX=20
+                    tY=cY
+                    bY=cY+10
+                    bb[0]=lX*s
+                    bb[1]=bY*s
+                    bb[2]=lX*s
+                    bb[3]=tY*s
+                    bb[4]=rX*s
+                    bb[5]=tY*s
+                    bb[6]=rX*s
+                    bb[7]=bY*s
+                    bb[8]=s*(lX+rX)/2.0
+                    bb[9]=s*bY
+                    bb[10]=s*(lX+rX)/2.0
+                    bb[11]=s*tY
+                    bb[12]=s*lX
+                    bb[13]=s*(tY+bY)/2.0
+                    bb[14]=s*rX
+                    bb[15]=s*(tY+bY)/2.0
+                    word_boxes.append(bb)
+                    word_trans.append(a)
+
+                cY+=11
             not_present = [w for w in skipped if w not in questions]
             diff = self.questions - len(self.qa)
             for w in not_present[:diff]:
