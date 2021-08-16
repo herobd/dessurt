470e30774081f3ec6b115341683036872ab4e83e
Brian Davis <herobd@gmail.com>
2021-08-16 17:05:54 -0600

    started on CDIP dataset

---
+++ datasets/cdip_qa.py
@@ -0,0 +1,338 @@
+import torch.utils.data
+import numpy as np
+import json
+#from skimage import io
+#from skimage import draw
+#import skimage.transform as sktransform
+import os
+import math, random, string
+from collections import defaultdict, OrderedDict
+from utils.funsd_annotations import createLines
+import timeit
+from .qa import QADataset
+from .synth_qadoc_dataset import addRead, breakLong
+
+import utils.img_f as img_f
+
+def collate(batch):
+    return {
+            'img': torch.cat([b['img'] for b in batch],dim=0),
+            'bb_gt': [b['bb_gt'] for b in batch], #torch.cat([b['bb_gt'] for b in batch],dim=0),
+            'imgName': [b['imgName'] for b in batch],
+            'scale': [b['scale'] for b in batch],
+            'cropPoint': [b['cropPoint'] for b in batch],
+            'transcription': [b['transcription'] for b in batch],
+            'metadata': [b['metadata'] for b in batch],
+            'form_metadata': [b['form_metadata'] for b in batch],
+            'questions': [b['questions'] for b in batch],
+            'answers': [b['answers'] for b in batch]
+            }
+
+
+class CDIPQA(QADataset):
+    """
+    Class for reading forms dataset and creating starting and ending gt
+    """
+
+
+    def __init__(self, dirPath=None, split=None, config=None, images=None):
+        super(CDIPQA, self).__init__(dirPath,split,config,images)
+
+        self.cache_resized = False
+
+        self.corruption_p = config['text_corruption'] if 'text_corruption' in config else 0
+
+        self.extra_np = 0.05
+
+        self.num_question_types=15
+        #question types
+        #0. Read from prompt (no highlight) including new lines (stops at block end) and draw where you read
+        # . Read from prompt (with highlight) including new lines (stops at block end) and draw where you read
+        #1. Read backwards from prompt (no highlight) including new lines (stops at block end) and draw where you read
+        # . Read backwards from prompt (with highlight) including new lines (stops at block end) and draw where you read
+        #2. Return blanked words (no highlight) and draw where it is "the [blank] chased the cat" > "cat"
+        # . Return blanked words (with highlight) and draw where it is "the [blank] chased the cat" > "cat"
+        #3. Return replaced word (no highlight) and draw where it is "the industrial chased the cat" > "cat"
+        # . Return replaced word (with highlight) and draw where it is "the industrial chased the cat" > "cat"
+        #4. Read line above (no highlight)and draw where it is. based on position, not just para/block
+        # . Read line above (with highlight) and draw where it is. based on position, not just para/block
+        #5. Read line below (no highlight)and draw where it is. based on position, not just para/block
+        # . Read line below (with highlight) and draw where it is. based on position, not just para/block
+        #6. bool is start of block (no highlight) (line) if no, hightlight next line
+        # . bool is start of block (with highlight) (line) if no, hightlight next line
+        #7. bool is end of block (no highlight) if no, hightlight next line
+        # . bool is end of block (with highlight) if no, hightlight next line
+        #8. draw the line this is in 
+        #9. draw the para this is in
+        #10. draw the block this is in
+        #=========
+        #11. draw where this text is 
+        #12. Read highlighted section
+        #13. Read highlighted section filling in masked word
+        #14. guess masked word (HARD!)
+
+        #input mask. 0 everywhere, 1 is highlight, -1 where removed
+        #  Multi-channel for multiple questions?
+
+        if images is not None:
+            self.images=images
+        else:
+            if 'overfit' in config and config['overfit']:
+                splitFile = 'overfit_split.json'
+            else:
+                splitFile = 'train_valid_test_split.json'
+            with open(os.path.join(dirPath,splitFile)) as f:
+                #if split=='valid' or split=='validation':
+                #    trainTest='train'
+                #else:
+                #    trainTest=split
+                readFile = json.loads(f.read())
+                if type(split) is str:
+                    toUse = readFile[split]
+                    imagesAndAnn = []
+                    for path in toUse['images']:
+                        name = path[path.rindex('/')+1:]
+                        imagesAndAnn.append( (name,os.path.join(dirPath,path+'.png'),os.path.join(dirPath,name+'.json')) )
+                elif type(split) is list:
+                    imagesAndAnn = []
+                    for spstr in split:
+                        toUse = readFile[spstr]
+                        for path in toUse['images']:
+                            name = path[path.rindex('/')+1:]
+                            imagesAndAnn.append( (name,os.path.join(dirPath,path+'.png'),os.path.join(dirPath,name+'.json')) )
+                else:
+                    print("Error, unknown split {}".format(split))
+                    exit()
+            self.images=[]
+            for imageName,imagePath,jsonPath in imagesAndAnn:
+                if os.path.exists(jsonPath):
+                    org_path = imagePath
+                    if self.cache_resized:
+                        path = os.path.join(self.cache_path,imageName+'.png')
+                    else:
+                        path = org_path
+
+                    rescale=1.0
+                    if self.cache_resized:
+                        rescale = self.rescale_range[1]
+                        if not os.path.exists(path):
+                            org_img = img_f.imread(org_path)
+                            if org_img is None:
+                                print('WARNING, could not read {}'.format(org_img))
+                                continue
+                            resized = img_f.resize(org_img,(0,0),
+                                    fx=self.rescale_range[1], 
+                                    fy=self.rescale_range[1], 
+                                    )
+                            img_f.imwrite(path,resized)
+                    self.images.append({'id':imageName, 'imagePath':path, 'annotationPath':jsonPath, 'rescaled':rescale ]})
+                else:
+                    print('{} does not exist'.format(jsonPath))
+                    print('No json found for {}'.format(imagePath))
+                    exit(1)
+        self.errors=[]
+
+
+
+    def parseAnn(self,ocr,s):
+        image_h=ocr['height']
+        image_w=ocr['width']
+        ocr=['blocks']
+        qa = self.makeQuestions(ocr,image_h,image_w)
+
+        qbbs=[]
+        t_id_to_bb={}
+        new_qa = []
+        for q,a,t_ids,in_mask,out_mask in qa
+            for t_id in t_ids:
+                if t_id in t_id_to_bb:
+                    bb_id = t_id_to_bb[t_id]
+                else:
+                    b,p,l,w = t_id
+                    inst = ocr[b]
+                    if p is not None:
+                        inst = inst[b]
+                        if l is not None:
+                            inst = inst[p]
+                            if w is not None:
+                                inst = inst[w]
+                    
+                    box = inst['box']
+                    #text = inst['text']
+                    
+
+                    lX,tY,rX,bY = box
+                    h=bY-tY
+                    w=rX-lX
+                    if h/w>5 and self.rotate: #flip labeling, since FUNSD doesn't label verticle text correctly
+                        assert False #Do i need to do this?
+                        #I don't know if it needs rotated clockwise or countercw, so I just say countercw
+                        bbs[j,0]=lX*s
+                        bbs[j,1]=bY*s
+                        bbs[j,2]=lX*s
+                        bbs[j,3]=tY*s
+                        bbs[j,4]=rX*s
+                        bbs[j,5]=tY*s
+                        bbs[j,6]=rX*s
+                        bbs[j,7]=bY*s
+                        #we add these for conveince to crop BBs within window
+                        bbs[j,8]=s*(lX+rX)/2.0
+                        bbs[j,9]=s*bY
+                        bbs[j,10]=s*(lX+rX)/2.0
+                        bbs[j,11]=s*tY
+                        bbs[j,12]=s*lX
+                        bbs[j,13]=s*(tY+bY)/2.0
+                        bbs[j,14]=s*rX
+                        bbs[j,15]=s*(tY+bY)/2.0
+                    else:
+                        bb = [lX*s, tY*s, rX*s, tY*s, rX*s, bY*s, lX*s, bY*s,
+                               s*lX, s*(tY+bY)/2.0, s*rX, s*(tY+bY)/2.0, s*(lX+rX)/2.0, s*tY, s*(rX+lX)/2.0, s*bY]  #we add these for conveince to crop BBs within window
+                        bb_id = len(qbbs)
+                        qbbs.append(bb)
+        qa_bbs = np.array(qbbs)
+
+
+        return qa_bbs, list(range(qa_bbs.shape[0])), None, {}, {}, qa
+
+
+    def getResponseBBIdList(self,queryId,annotations):
+        if self.split_to_lines:
+            return annotations['linking'][queryId]
+        else:
+            boxes=annotations['form']
+            cto=[]
+            boxinfo = boxes[queryId]
+            for id1,id2 in boxinfo['linking']:
+                if id1==queryId:
+                    cto.append(id2)
+                else:
+                    cto.append(id1)
+            return cto
+
+    def makeQuestions(self,ocr,image_h,image_w):
+        wordmap=None
+        qa=[]
+        for i in range(self.questions):
+            question_type = random.randrange(self.num_question_types)
+            if question_type ==0:
+                #0. Read from prompt (no highlight) including new lines (stops at block end) and draw where you read
+                # . Read from prompt (with highlight) including new lines (stops at block end) and draw where you read
+                use_highlight = random.random()<0.5
+                if wordmap is None:
+                    wordmap = makeWordmap(ocr)
+                start_word_idx = random.randrange(len(wordmap))
+                goal_prompt_len = random.randrange(self.min_read_start,self.max_qa_len+1)
+                
+                words_in_prompt=[start_word_idx]
+                start_word = wordmap[start_word_idx]
+                prompt = ocr[start_word[0],start_word[1],start_word[2],start_word[3]]['text']
+                start_block = start_word[0]
+                next_word_idx = start_word_idx+1
+                next_word = wordmap[next_word_idx]
+                next_text = ocr[next_word[0]]['paragraphs'][next_word[1]]['lines'][next_word[2]['text']
+                while len(prompt)+1+len(next_text)<self.max_qa_len and next_word[0]==start_block:
+                    prompt+=' '+next_text
+                    words_in_prompt.append(next_word_idx)
+                    next_word_idx = next_word_idx+1
+                    next_word = wordmap[next_word_idx]
+                    next_text = ocr[next_word[0]]['paragraphs'][next_word[1]]['lines'][next_word[2]['text']
+                    if len(prompt)>=goal_prompt_len:
+                        break
+                if prompt>self.max_qa_len:
+                    prompt = prompt[-self.max_qa_len:]A
+
+                if next_word[0]!=start_block:
+                    response='[ end ]'
+                    words_in_response=[]
+                else:
+                    goal_response_len = self.max_qa_len
+                    response=next_text
+                    words_in_response=[next_word_idx]
+                    next_word_idx = start_word_idx+1
+                    next_word = wordmap[next_word_idx]
+                    next_text = ocr[next_word[0]]['paragraphs'][next_word[1]]['lines'][next_word[2]['text']
+                    while len(response)+1+len(next_text)<self.max_qa_len and next_word[0]==start_block:
+                        response+=' '+next_text
+                        words_in_response.append(next_word_idx)
+                        next_word_idx = next_word_idx+1
+                        next_word = wordmap[next_word_idx]
+                        next_text = ocr[next_word[0]]['paragraphs'][next_word[1]]['lines'][next_word[2]['text']
+                        if len(response)>=goal_response_len:
+                            break
+                    if response>self.max_qa_len:
+                        response = response[-self.max_qa_len:]A
+                if use_highlight:
+                    question = 'r0~'
+                    indexes =  [wordmap[i] for i in words_in_prompt]
+                    #inmask = highlightAll(image_h,image_w,ocr,indexes)
+                    inmask = allBoxes(ocr,indexes)
+                else:
+                    question = 're~'
+                    inmask = []
+
+                #outmask = highlightAll(image_h,image_w,ocr,[wordmap[i] for i in words_in_response])
+                outmask = allBoxes(ocr,indexes)
+
+                qa.append([question+prompt,response,[wordmap[i] for i in words_in_prompt+words_in_response],inmask,outmask])
+
+                #1. Read backwards from prompt (no highlight) including new lines (stops at block end) and draw where you read
+                # . Read backwards from prompt (with highlight) including new lines (stops at block end) and draw where you read
+                #2. Return blanked words (no highlight) and draw where it is "the [blank] chased the cat" > "cat"
+                # . Return blanked words (with highlight) and draw where it is "the [blank] chased the cat" > "cat"
+                #3. Return replaced word (no highlight) and draw where it is "the industrial chased the cat" > "cat"
+                # . Return replaced word (with highlight) and draw where it is "the industrial chased the cat" > "cat"
+                #4. Read line above (no highlight)and draw where it is. based on position, not just para/block
+                # . Read line above (with highlight) and draw where it is. based on position, not just para/block
+                #5. Read line below (no highlight)and draw where it is. based on position, not just para/block
+                # . Read line below (with highlight) and draw where it is. based on position, not just para/block
+                #6. bool is start of block (no highlight) (line) if no, hightlight next line
+                # . bool is start of block (with highlight) (line) if no, hightlight next line
+                #7. bool is end of block (no highlight) if no, hightlight next line
+                # . bool is end of block (with highlight) if no, hightlight next line
+                #8. draw the line this is in 
+                #9. draw the para this is in
+                #10. draw the block this is in
+                #=========
+                #11. draw where this text is 
+                #12. Read highlighted section
+                #13. Read highlighted section filling in masked word
+                #14. guess masked word (HARD!)
+        return qa
+
+#def highlightAll(image_h,image_w,ocr,indexes,value=1):
+#    mask = torch.FloatTensor(image_h,image_w).fill_(0)
+#    for idx in indexes:
+#        if idx[3] is not None:
+#            l,t,r,b = ocr[idx[0]]['paragraphs'][idx[1]]['lines'][idx[2]]['words'][idx[3]]['box']
+#        elif idx[2] is not None:
+#            l,t,r,b = ocr[idx[0]]['paragraphs'][idx[1]]['lines'][idx[2]]['box']
+#        elif idx[1] is not None:
+#            l,t,r,b = ocr[idx[0]]['paragraphs'][idx[1]]['box']
+#        else:
+#            l,t,r,b = ocr[idx[0]]['box']
+#
+#        mask[t:b+1,l:r+1]=value
+#    return mask
+
+def allMasks(ocr,indexes,value=1):
+    ret=[]
+    for idx in indexes:
+        if idx[3] is not None:
+            bb = ocr[idx[0]]['paragraphs'][idx[1]]['lines'][idx[2]]['words'][idx[3]]['box']
+        elif idx[2] is not None:
+            bb = ocr[idx[0]]['paragraphs'][idx[1]]['lines'][idx[2]]['box']
+        elif idx[1] is not None:
+            bb = ocr[idx[0]]['paragraphs'][idx[1]]['box']
+        else:
+            bb = ocr[idx[0]]['box']
+        ret.append(bb)
+    return ret
+
+def makeWordmap(ocr):
+    wordmap=[]
+    for b,block in enumerate(ocr):
+        for p,para in enumerate(block['paragraphs']):
+            for l,line in enumerate(para['lines']):
+                for w in range(len(line['words'])):
+                    wordmap.append((b,p,l,w))
+    return wordmap
---
+++ datasets/test_cdip_qa.py
@@ -0,0 +1,107 @@
+from datasets import cdip_qa
+import math
+import sys
+from matplotlib import pyplot as plt
+from matplotlib import gridspec
+from matplotlib.patches import Polygon
+import numpy as np
+import torch
+import utils.img_f as cv2
+
+widths=[]
+
+def display(data):
+    batchSize = data['img'].size(0)
+    #mask = makeMask(data['image'])
+    for b in range(batchSize):
+        #print (data['img'].size())
+        img = (1-data['img'][b].permute(1,2,0))/2.0
+        #label = data['label']
+        #gt = data['gt'][b]
+        #print(label[:data['label_lengths'][b],b])
+        print(data['imgName'][b])
+        #if data['spaced_label'] is not None:
+        #    print('spaced label:')
+        #    print(data['spaced_label'][:,b])
+        for bb,text in zip(data['bb_gt'][b],data['transcription'][b]):
+            print('ocr: {} {}'.format(text,bb))
+        #print('questions: {}'.format(data['questions'][b]))
+        #print('answers: {}'.format(data['answers'][b]))
+        print('questions and answers')
+        for q,a in zip(data['questions'][b],data['answers'][b]):
+            print(q+' : '+a)
+
+        #widths.append(img.size(1))
+        
+        draw=True
+        if draw :
+            #cv2.imshow('line',img.numpy())
+            #cv2.imshow('mask',maskb.numpy())
+            #cv2.imwrite('out/mask{}.png'.format(b),maskb.numpy()*255)
+            #cv2.imwrite('out/fg_mask{}.png'.format(b),fg_mask.numpy()*255)
+            #cv2.imwrite('out/img{}.png'.format(b),img.numpy()*255)
+            #cv2.imwrite('out/changed_img{}.png'.format(b),changed_img.numpy()*255)
+            plt.imshow(img.numpy()[:,:,0], cmap='gray')
+            plt.show()
+
+            #cv2.waitKey()
+
+            cv2.imwrite('testsinglesize_1024.png',img.numpy()[:,:,0])
+
+        #fig = plt.figure()
+
+        #ax_im = plt.subplot()
+        #ax_im.set_axis_off()
+        #if img.shape[2]==1:
+        #    ax_im.imshow(img[0])
+        #else:
+        #    ax_im.imshow(img)
+
+        #plt.show()
+    print('batch complete')
+
+
+if __name__ == "__main__":
+    if len(sys.argv)>1:
+        dirPath = sys.argv[1]
+    else:
+        dirPath = '../data/CDIP'
+    if len(sys.argv)>2:
+        start = int(sys.argv[2])
+    else:
+        start=0
+    if len(sys.argv)>3:
+        repeat = int(sys.argv[3])
+    else:
+        repeat=1
+    data=cdip_qa.CDIPQA(dirPath=dirPath,split='train',config={
+        'rescale_range':[0.38,0.5],
+        'crop_params': {
+            "crop_size":[1152,768],
+            "pad":0,
+            "rot_degree_std_dev": 1
+            },
+        'questions':50,
+        'max_qa_len': 26
+
+})
+
+    dataLoader = torch.utils.data.DataLoader(data, batch_size=1, shuffle=True, num_workers=0, collate_fn=cdip_qa.collate)
+    dataLoaderIter = iter(dataLoader)
+
+        #if start==0:
+        #display(data[0])
+    for i in range(0,start):
+        print(i)
+        dataLoaderIter.next()
+        #display(data[i])
+    try:
+        while True:
+            #print('?')
+            display(dataLoaderIter.next())
+    except StopIteration:
+        print('done')
+
+    #print('width mean: {}'.format(np.mean(widths)))
+    #print('width std: {}'.format(np.std(widths)))
+    #print('width max: {}'.format(np.max(widths)))
