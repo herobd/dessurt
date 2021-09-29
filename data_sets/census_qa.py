53b50bcc0718ccf10a4a683c6c874a3a539a6e97
Brian Davis <herobd@gmail.com>
2021-09-29 16:54:34 -0600

    Census dataset seems to be working, but need to figure out image size

--- data_sets/census_qa.py
+++ data_sets/census_qa.py
@@ -9,7 +9,7 @@ import math, random, string, re
 from collections import defaultdict, OrderedDict
 from utils.funsd_annotations import createLines
 import timeit
-from data_sets.para_qa_dataset import ParaQADataset, collate
+from data_sets.record_qa import RecordQA, collate
 
 import utils.img_f as img_f
 
@@ -45,7 +45,7 @@ class CensusQA(RecordQA):
                     imagesAndAnn = []
                     for image_path,json_path in instances:
                         try:
-                            name = image_path[path.rindex('/')+1:]
+                            name = image_path[image_path.rindex('/')+1:]
                         except ValueError:
                             name = image_path
                         imagesAndAnn.append( (name,os.path.join(dirPath,image_path),os.path.join(dirPath,json_path)) )
@@ -82,8 +82,10 @@ class CensusQA(RecordQA):
                 #    print('No json found for {}'.format(imagePath))
                 #    #exit(1)
 
-        self.all_fields=set(['line no.','household','name','previous','race','relationship','sex'])
+        self.all_fields=set(['line no.','household','name','previous','race','relationship','sex', 'given name', 'age', 'birthplace'])
         self.id_fields=set(['line no.','name'])
+        self.next_name_ids = ['given name','name']
+        self.main_id_field = 'line no.'
 
 
     def parseAnn(self,data,s):
@@ -93,18 +95,33 @@ class CensusQA(RecordQA):
         else:
             recognition = None
 
-        entries =[
-                {
-                    'line no.': entry['LINE_NBR'],
-                    'household': entry['HOUSEHOLD_ID'],
-                    'name': entry['PR_NAME'],
-                    'previous': entry['PR_PREV_RESIDENCE_PLACE'],
-                    'race': entry['PR_RACE_OR_COLOR'],
-                    'relationship': entry['PR_RELATIONSHIP_TO_HEAD'],
-                    'sex': entry['PR_SEX_CODE'],
-
-                    } for entry in data]
-        qa, qa_bbs = self.makeQuestions(s,entries)
+        #entries =[
+        #        {
+        #            'line no.': entry['LINE_NBR'],
+        #            'household': entry['HOUSEHOLD_ID'],
+        #            'name': entry['PR_NAME'],
+        #            'previous': entry['PR_PREV_RESIDENCE_PLACE'] if 'PR_PREV_RESIDENCE_PLACE' in entry else None,
+        #            'race': entry['PR_RACE_OR_COLOR'],
+        #            'relationship': entry['PR_RELATIONSHIP_TO_HEAD'],
+        #            'sex': entry['PR_SEX_CODE'],
+        #            'given name': entry['PR_NAME_GN'],
+        #            'age': entry['PR_AGE'],
+        #            'birthplace': entry['PR_BIRTH_PLACE']
+        #            } for entry in data]
+        name_to_id = {
+                    'line no.': 'LINE_NBR',
+                    'household': 'HOUSEHOLD_ID',
+                    'name': 'PR_NAME',
+                    'previous': 'PR_PREV_RESIDENCE_PLACE',
+                    'race': 'PR_RACE_OR_COLOR',
+                    'relationship': 'PR_RELATIONSHIP_TO_HEAD',
+                    'sex': 'PR_SEX_CODE',
+                    'given name': 'PR_NAME_GN',
+                    'age': 'PR_AGE',
+                    'birthplace': 'PR_BIRTH_PLACE'
+                    } 
+        entries = [ {key:entry[name_to_id[key]] if name_to_id[key] in entry else None for key in self.all_fields} for entry in data]
+        qa = self.makeQuestions(s,entries)
 
 
         return np.zeros(0), [], None, {}, {}, qa
