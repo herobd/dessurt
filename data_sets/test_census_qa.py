44b480ead00d798c0bd4592b972d98ef497c7db9
Brian Davis <herobd@gmail.com>
2021-09-30 11:57:24 -0600

    tested cesus with new resolution

--- data_sets/test_census_qa.py
+++ data_sets/test_census_qa.py
@@ -94,10 +94,10 @@ if __name__ == "__main__":
     else:
         repeat=1
     data=census_qa.CensusQA(dirPath=dirPath,split='train',config={
-        'rescale_range': [1.2,1.2],
+        'rescale_range': [1,1.15],
         'rescale_to_crop_size_first': True,
         'crop_params': {
-            "crop_size":[960,1280],#5953x4504  6164x4631  6234x4769
+            "crop_size":[1056,1408],#5953x4504  6164x4631  6234x4769
             "pad":0,
             "rot_degree_std_dev": 1
             },
