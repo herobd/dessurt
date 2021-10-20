d7e571eb7af1a33d2e5886d2015d4aed0b5c460f
Brian Davis <herobd@gmail.com>
2021-10-20 11:17:50 -0600

    tweaks

--- data_sets/wiki_text.py
+++ data_sets/wiki_text.py
@@ -49,4 +49,8 @@ def getWikiArticle():
     if len(paras)>0:
         return paras
     else:
+        print('blank article:')
+        print(text)
+        print('------------')
+        print(_text_data[instance_i]['text'])
         return getWikiArticle()
