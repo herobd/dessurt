32b8056ac4c3d2608673d67466e46ef3431d8ded
Brian Davis <herobd@gmail.com>
2022-01-07 15:33:20 -0700

    tweaks and stuff

--- data_sets/form_qa.py
+++ data_sets/form_qa.py
@@ -212,7 +212,7 @@ class FormQA(QADataset):
         if self.train:
             if use_json:
                 self.q_types = {
-                        'full_json': 4,
+                        'full_json': 10,
                         'np':0.2,
                         #'all':1.0,
                         #'class-link':1.3,
@@ -242,7 +242,7 @@ class FormQA(QADataset):
                         'count-tables':0.01
                         }
                 self.q_types_only_table = {
-                        'full_json': 4,
+                        'full_json': 11,
                         'np':0.2,
                         #'all':1.0,
                         #'class-link':1.3,
